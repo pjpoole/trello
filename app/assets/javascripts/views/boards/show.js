68bb4bccb065a30b2953a35590c065b92b3a9772
Peter Poole <peter.j.poole@gmail.com>
2014-12-31 16:53:19 -0500

    More css tweaks.

--- app/assets/javascripts/views/boards/show.js
+++ app/assets/javascripts/views/boards/show.js
@@ -4,6 +4,17 @@ Trello.Views.BoardShow = Backbone.View.extend({
 
   render: function () {
     this.$el.html(this.template({ board: this.model }));
+
+    if (this.model.lists) {
+      var boardUl = this;
+
+      this.model.lists.forEach(function (list) {
+        var view = new Trello.Views.ListShow({ model: list });
+
+        board.$el.append(view.render().$el);
+      });
+    }
+
     return this;
   }
 });
